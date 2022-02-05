409b4f2c03f38c96c918cb40a21321f0304f12b4
Erwin Madjus <erwinmadjusbusiness@gmail.com>
2022-02-04 18:12:25 -0800

    Lib and Code that Fails

--- FinalSkillDemo.java
+++ FinalSkillDemo.java
@@ -1,3 +1,13 @@
-public class FinalSkillDemo {} 
+public class FinalSkillDemo {
+    public static int add(int n, int m) {
+        if (n > m) {
+            return n + m; 
+        }
+        else if (n < m) {
+            return m - n; 
+        }
+        else return 10; 
+    }
+} 
 
 // I will be editing this on Github Desktop and VSCode
---
+++ FinalSkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class FinalSkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(5, FinalSkillDemo.add(3,1));
+    }
+
+}
